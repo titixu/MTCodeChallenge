0c66e057f07743d6aabefdd388f63a851b1d7433
sam xu <titianxu@gmail.com>
2019-07-24 13:03:44 +1000

    Initial Commit

---
+++ MTCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTCodeChallenge
+//
+//  Created by Sam Xu on 24/7/19.
+//  Copyright © 2019 Sam Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
